3195b2f0bba63c1a8383d6a1f501851e171edaf4
Jason Kao <jkao1@stuy.edu>
2016-09-16 12:28:47 -0400

    added new test cases

--- hw06-HomeworkTest.java
+++ hw06-HomeworkTest.java
@@ -7,6 +7,10 @@ public class HomeworkTest{
     	System.out.println(celsiusToFahrenheit(100.0));
     	System.out.println("The test for 212degF is:");
     	System.out.println(fahrenheitToCelsius(212.0));
+    	System.out.println("The test for 72.5degC is:");
+	System.out.println(celsiusToFahrenheit(72.5));
+	System.out.println("The test for 123.456degF is:");
+	System.out.println(fahrenheitToCelsius(123.456));
     }
 
     public static double celsiusToFahrenheit(double x){
