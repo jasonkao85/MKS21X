99f26a9422aca342df295f1cd2589b91fb4e4a08
jkao1 <jkao1@stuy.edu>
2016-09-17 14:49:07 -0400

    added hw01

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{
+    public static void main(String[]args){
+        System.out.println("Hello, my name is Inigo Montoya, You killed my");
+        System.out.println("father, prepare to die!");
+    }
+}
