b0056f09ab19f493eb52725bbef59b243bfbbe1f
jkao1 <jkao1@stuy.edu>
2016-10-01 16:30:42 -0400

    deleted

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld{
-    public static void main(String[]args){
-        System.out.println("Hello, my name is Inigo Montoya, You killed my");
-        System.out.println("father, prepare to die!");
-    }
-}
