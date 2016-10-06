10ca6569532ebd33f5cecf9318460d7dcbbde3ca
jkao1 <jkao1@stuy.edu>
2016-10-06 18:43:13 -0400

    added 1006-myUpper

---
+++ 1006-myUpper/myUpper.java
@@ -0,0 +1,28 @@
+public class myUpper{
+
+    public static void main(String[]args){
+	
+     	System.out.println(myUpper("Jason Kao"));
+		
+    }
+	
+    public static String myUpper(String str) {
+	
+	String s = "";
+	int i = 0;
+		
+	while (i < str.length()) {
+	    if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z'){
+		s += (char) (str.charAt(i) - 'a' + 'A');
+	    } else {
+		s += str.charAt(i);
+	    }
+	    i++;
+	}
+		
+	return s;
+    }
+	
+}
+
+    
