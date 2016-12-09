1ede578d4673e941339b5446324dbccd1d60276b
jkao1 <jkao1@stuy.edu>
2016-12-09 17:55:15 -0500

    good version

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -2,9 +2,10 @@ import java.lang.RuntimeException;
 
 public class Barcode implements Comparable<Barcode>{
 
-    // instance variables
     private String _zip;
     private int _checkDigit;
+
+    private static String[] scheme = { "||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::" };
 
     public Barcode(String zip) {
 	if (!zip.matches("[0-9]{5}")) {
@@ -12,13 +13,13 @@ public class Barcode implements Comparable<Barcode>{
 	    throw new RuntimeException();
 	}		
 	_zip = zip;
-	_checkDigit = checkSum(_zip) % 10;
+	_checkDigit = this.checkSum(_zip) % 10;
     }
 
-    private int checkSum(String zip){
+    private static int checkSum(String n){
 	int s = 0;
-	for (int i = 0; i < _zip.length(); i++) {
-	    s += _zip.charAt(i) - '0';
+	for (int i = 0; i < n.length(); i++) {
+	    s += n.charAt(i) - '0';
 	}
 	return s;
     }
@@ -26,20 +27,7 @@ public class Barcode implements Comparable<Barcode>{
     public String toString(){
 	String o = _zip + _checkDigit + "  |";
 	for (int i = 0; i <= _zip.length(); i++) {
-	    char c = (_zip + _checkDigit).charAt(i);
-	    switch (c) {
-	    case '0': o += "||:::"; break;	           	    
-	    case '1': o += ":::||"; break;
-	    case '2': o += "::|:|"; break;
-	    case '3': o += "::||:"; break;
-	    case '4': o += ":|::|"; break;
-	    case '5': o += ":|:|:"; break;
-	    case '6': o += ":||::"; break;
-	    case '7': o += "|:::|"; break;
-	    case '8': o += "|::|:"; break;
-	    case '9': o += "|:|::"; break;
-	    default: break;
-	    }
+	    o += scheme[(_zip + _checkDigit).charAt(i) - '0'];
 	}
 	return o + "|";
     }
@@ -49,33 +37,44 @@ public class Barcode implements Comparable<Barcode>{
     }
 
     public static String toCode(String zip) {
-	String o = "|";
-	int s = 0;
+
 	zip += checkSum(zip) % 10;
+	
+        String o = "|";
 	for (int i = 0; i < zip.length(); i++) {
-	    char c = (zip).charAt(i);
-	    switch (c) {
-	    case '0': o += "||:::"; break;	           	    
-	    case '1': o += ":::||"; break;
-	    case '2': o += "::|:|"; break;
-	    case '3': o += "::||:"; break;
-	    case '4': o += ":|::|"; break;
-	    case '5': o += ":|:|:"; break;
-	    case '6': o += ":||::"; break;
-	    case '7': o += "|:::|"; break;
-	    case '8': o += "|::|:"; break;
-	    case '9': o += "|:|::"; break;
-	    default: break;
-	    }
-	    s += c - '0';
-	}
+	    o += scheme[(zip).charAt(i) - '0']; // doesn't need this.method() b/c method is static  
+        }
 	return o + "|";
     }
 
-    public static String toZip(String code) {
-	String o = "";
-	for (int i = 0; i < code.length() - 5; i += 5) {
-	    System.out.println(code.charAt(i));
+    private static int schemeDigit(String s) {
+	for (int i = 0; i < scheme.length; i++) {
+	    if (scheme[i].equals(s)) return i;
 	}
+        throw new IllegalArgumentException("Encoded ints are invalid or non-barcode characters are used.");
     }
+
+    private static int checkSumCode(String code) {
+        int s = 0;
+	for (int i = 0; i < code.length() - 4; i += 5) {
+	    s += schemeDigit(code.substring(i,i+5));       
+	}
+	return s;
+    }
+
+    public static String toZip(String code) {
+	if (code.charAt(0) != '|' || code.charAt(code.length()-1) != '|') {
+	    throw new IllegalArgumentException("Left and rightmost characters are not '|'.");
+	} else if (code.length() != 32) {
+	    throw new IllegalArgumentException("Length of barcode is not 32.");
+	} else if (checkSumCode(code.substring(1,26))%10 != schemeDigit(code.substring(26,31))) {
+	    throw new IllegalArgumentException("Checksum is invalid.");
+	}
+	String o = "";
+        for (int i = 1; i < code.length() - 6; i += 5) {
+	    o += schemeDigit(code.substring(i,i+5));       
+	}
+	return o + checkSum(o) % 10;
+    }
+
 }
--- 03Barcode/Driver.java
+++ 03Barcode/Driver.java
@@ -2,10 +2,34 @@ public class Driver {
 
     public static void main(String[]args) {
 
-	Barcode a = new Barcode("12345");
-        System.out.println(Barcode.toZip("|||:::|::|::|::|:|:|::::|||::|:|"));
-	    
+	String[] goodStuffs = {
+	    "|||:::|::|::|::|:|:|::::|||::|:|",
+	    "|||:::|::|::|::|:|:|::::|||::|:|",
+	    "|||:::|::|::|::|:|:|::::|||::|:|",
+	    "|||:::|::|::|::|:|:|::::|||::|:|",
+	};
 
+	for (String gs : goodStuffs) {
+	    System.out.println("\n# SHOULD BE OK -------");
+	    System.out.println(Barcode.toZip(gs));
+	}	
+
+	String[] errors = {
+	    "|||:::|::|::|::|:|:|::::|||:|::|", //checksum
+	    "|||:::|::|::|::|:|:|::::|:|::|:|", //encoded ints or checksum
+	    "|||:::|::|::|?:|:|:|::::|||::|:|", //invalid char
+	    "|||:::|::|::|::|:|:|:::|||::|:|", //length
+	    ":||:::|::|::|::|:|:|::::|||::|:|", // left/rightmost
+	};
+
+	for (String error : errors) {
+	    System.out.println("\n# SHOULD BE ERROR ---------");
+	    try { System.out.println(Barcode.toZip(error)); }
+	    catch (IllegalArgumentException e) {
+		e.printStackTrace();
+	    }
+	}
+	    
     }
 
 }
