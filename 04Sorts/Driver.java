19795a813c344119760853c13525cfa28f8fd872
jkao1 <jasonkao85@gmail.com>
2016-12-13 03:25:07 -0500

    3

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -3,8 +3,6 @@ public class Driver {
     public static void main(String[] args) {
 	int[] hi = { 10, 2, 30, 14, 12, 200 };
 	Sorts.selectionSort(hi);
-	for (int i : hi) {
-	    System.out.println(i);
-	}
+        
     }
 }
