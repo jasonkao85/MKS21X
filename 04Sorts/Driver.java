b62a843b44b18d640ff13f5e7639f4ee0f0f3ccd
jkao1 <jasonkao85@gmail.com>
2016-12-14 02:34:16 -0500

    3

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -1,9 +1,23 @@
 public class Driver {
-    public static void main(String[] args) {
-	int[] hi = { 10, 2, 30, 14, 12, 200 };
-	Sorts.insertionSort(hi);        
-	for (int i : hi) {
-	    System.out.println(i);
-	}
+    
+    public static void main(String[] args) {	
+	int[] a = { 10, 3, 1, 5, 19, 10 };
+	System.out.println("-----SELECTION SORT-----");
+	Sorts.selectionSort(a);
+	System.out.println(toString(a));
+
+	int[] b = { 10, 3, 1, 5, 19, 10 };
+	System.out.println("-----INSERTION SORT-----");
+	Sorts.insertionSort(b);    
+        System.out.println(toString(b));
     }
+
+    public static String toString(int[] ary) {
+	String o = "[ ";
+	for (int i : ary) {
+	    o += i + ", ";
+	}
+	return o.substring(0,o.length()-2) + " ]";
+    }
+
 }
