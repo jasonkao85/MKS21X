b4372bf6e66a736aaf99fd3565561c76ba24e86f
jkao1 <jasonkao85@gmail.com>
2016-12-15 02:26:49 -0500

    made driver better

--- 04Sorts/Driver.java
+++ 04Sorts/Driver.java
@@ -1,20 +1,20 @@
 public class Driver {
     
-    public static void main(String[] args) {	
+    public static void main(String[] args) {
+	
 	int[] a = { 10, 3, 1, 5, 19, 10 };
-	System.out.println("-----SELECTION SORT-----");
-	Sorts.selectionSort(a);
+        int x = Integer.parseInt(args[0]);
+	String sortType = "whoops";
+	switch (x) {
+	case 0: Sorts.selectionSort(a);
+	    sortType = "SELECTION"; break;
+	case 1: Sorts.insertionSort(a);
+	    sortType = "INSERTION"; break;
+	case 2: Sorts.bubbleSort(a);
+	    sortType = "BUBBLE"; break;
+    }
+	System.out.println("-----"+sortType+" SORT-----");
 	System.out.println(toString(a));
-
-	int[] b = { 10, 3, 1, 5, 19, 10 };
-	System.out.println("-----INSERTION SORT-----");
-	Sorts.insertionSort(b);    
-        System.out.println(toString(b));
-
-	int[] c = { 10, 3, 1, 5, 19, 10 };
-	System.out.println("-----BUBBLE SORT-----");
-	Sorts.bubbleSort(c);
-	System.out.println(toString(c));
     }
 
     public static String toString(int[] ary) {
