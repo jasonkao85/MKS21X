8454a4b4f790b17551b33ed7f41f477ccba9ad87
jkao1 <jkao1@stuy.edu>
2016-12-21 21:52:16 -0500

    buttons work, have to arrange nicely now

--- 05Temperature/Temperature.java
+++ 05Temperature/Temperature.java
@@ -1,16 +1,55 @@
-public class Temperature {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Temperature extends JFrame implements ActionListener {
+
+    private Container pane;
+    private JTextField cels, fahr;
+
+    public Temperature() {
+	this.setTitle("Temperature Converter");
+	this.setSize(600,600);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new FlowLayout());
+
+	JButton cToF = new JButton("cToF");
+	cToF.addActionListener(this);
+	cToF.setActionCommand("cToF");
+	JButton fToC = new JButton("fToC");
+	fToC.addActionListener(this);
+	fToC.setActionCommand("fToC");
+	
+	cels = new JTextField(5);
+	fahr = new JTextField(5);
+	
+	pane.add(cToF);
+	pane.add(fToC);
+	pane.add(cels);
+	pane.add(fahr);
+    }
+  
+    public void actionPerformed(ActionEvent e) {
+	String event = e.getActionCommand();
+
+	if (event.equals("cToF")) {
+	    double c = Double.parseDouble(cels.getText());
+	    c = (9.0 / 5) * c + 32;
+	    fahr.setText(String.valueOf(c));
+	}
+	if (event.equals("fToC")) {
+	    double f = Double.parseDouble(cels.getText());
+	    f = (5.0 / 9) * (f - 32);
+	    fahr.setText(String.valueOf(f));
+	}
+    }
 
     public static void main(String[] args) {
-	System.out.println(CtoF(100.0));
-	System.out.println(FtoC(212.0));
-    }
-
-    public static double CtoF(double cels) {
-	return (9.0 / 5) * cels + 32;
-    }
-
-    public static double FtoC(double fahr) {
-	return (5.0 / 9) * (fahr - 32);
+	Temperature g = new Temperature();
+	g.setVisible(true);
     }
 
 }
